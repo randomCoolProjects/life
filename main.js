a3eab41e11c862978c1bf98a3fe2a17298bbb6a6
randomCoolProjects <43590176+randomCoolProjects@users.noreply.github.com>
2019-04-05 18:18:54 -0300

    v 1.1

--- main.js
+++ main.js
@@ -12,10 +12,15 @@ const pbar          = select(".progress-bar");
 const livedPercent  = select("#lived");
 const toliveYears   = select("#tolive");
 const tolivePercent = select("#tolivep");
+const death         = select("#death");
 const resultPanel   = select("#result");
 
 var years = 0;
 var percent = 0;
+
+var today = new Date();
+var year = today.getFullYear();
+var deathYear = 0;
 
 btn.onclick = function()
 {
@@ -31,6 +36,18 @@ btn.onclick = function()
 
     pbar.style = 'width: ' + percent + '%;';
     pbar.innerText = percent + '%';
+
+    deathYear = year + toLive;
+
+    var dd = today.getDay();
+    var mm = today.getMonth();
+    if (dd < 10)
+    dd = '0' + dd;
+    if (mm < 10)
+    mm = '0' + mm;
+    death.innerText = ("Você irá morrer em " + dd + "/" + mm + "/" + deathYear + " às " + today.getHours() + " horas!");
+    if (today.getHours() < 8)
+    death.innerText += "\n(Você nem irá acordar)";
 
     resultPanel.className = '';
 }
