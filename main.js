632cdf62a46587e71e5c90d4f7b706a2224c70a7
randomCoolProjects <43590176+randomCoolProjects@users.noreply.github.com>
2019-04-06 08:22:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ const resultPanel   = select("#result");
 var years = 0;
 var percent = 0;
 
-var today = new Date();
+/*var today = new Date();
 var year = today.getFullYear();
-var deathYear = 0;
+var deathYear = 0;*/
 
 btn.onclick = function()
 {
@@ -37,7 +37,7 @@ btn.onclick = function()
     pbar.style = 'width: ' + percent + '%;';
     pbar.innerText = percent + '%';
 
-    deathYear = year + toLive;
+    /*deathYear = year + toLive;
 
     var dd = today.getDay();
     var mm = today.getMonth();
@@ -47,7 +47,7 @@ btn.onclick = function()
     mm = '0' + mm;
     death.innerText = ("Você irá morrer em " + dd + "/" + mm + "/" + deathYear + " às " + today.getHours() + " horas!");
     if (today.getHours() < 8)
-    death.innerText += "\n(Você nem irá acordar)";
+    death.innerText += "\n(Você nem irá acordar)";*/
 
     resultPanel.className = '';
 }
